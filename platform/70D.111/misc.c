dbbc95b413b283a5f3068e45467a8a94994ee2d1
Giovanni Condello <condellog@gmail.com>
2014-04-26 19:47:53 +0200

    Add files that were missing in commit 53074cf1627ebc67b653ec992f328a6eb956fb0f

---
+++ platform/70D.111/misc.c
@@ -0,0 +1,11 @@
+// misc functions specific to 70D
+
+#include <dryos.h>
+#include <property.h>
+#include <bmp.h>
+#include <config.h>
+#include <consts.h>
+#include <lens.h>
+
+int new_LiveViewApp_handler = 0xff123456;
+
